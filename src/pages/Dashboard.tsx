6d7c5ba9e50e9c57efaaa6db68441e0edfbe283f
oluwabukola2610 <roheemohmudashir@gmail.com>
2023-08-23 12:53:14 +0100

    updated styles

--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -28,15 +28,15 @@ const Dashboard = () => {
   return (
     <main className="mx-auto max-w-[1640px] flex flex-col gap-8 p-3 h-screen bg-[#F5F5F9] overflow-y-scroll">
       <div className="grid grid-cols-1 lg:grid-cols-[1fr,370px] gap-4 ">
-        <article className="flex flex-col space-y-4  ">
-          <div className="flex flex-col md:flex-row items-center text-center md:text-start space-x-12 p-3 shadow-[0_3px_10px_rgb(0,0,0,0.2)] bg-white">
-            <span className="space-y-6">
+        <article className="flex flex-col space-y-7  ">
+          <div className="flex flex-col md:flex-row items-center text-center md:text-start justify-between px-3 shadow-[0_3px_10px_rgb(0,0,0,0.2)] bg-white">
+            <span className="space-y-4">
               <h1 className="font-bold text-2xl">Hello Temi 🎉</h1>
               <p className="max-w-xs text-gray-500">
                 Get things done and start your day more productive
               </p>
             </span>
-            <img src={workmode} alt="" className="flex w-[200px] h-[200px]" />
+            <img src={workmode} alt="" className="flex w-[170px] h-[170px]" />
           </div>
           <div className="grid grid-cols-2 md:grid-cols-4 gap-12">
             {Taskdata.map((task, i) => (
@@ -60,7 +60,7 @@ const Dashboard = () => {
           </span>
           <div className="border-b p-1">
             <p className="text-gray-600 font-medium">Congratulation Flora 🎉</p>
-            <div className="flex justify-between py-2">
+            <div className="flex justify-between py-1">
               <p className="text-gray-400">
                 You have completed your task for the week
               </p>
@@ -69,7 +69,7 @@ const Dashboard = () => {
           </div>
           <div className="border-b p-1">
             <p className="text-gray-600 font-medium">You have a pending Task</p>
-            <div className="flex justify-between py-2">
+            <div className="flex justify-between py-1">
               <p className="text-gray-400">5 hours ago</p>
               <p className="text-gray-400">Today</p>
             </div>{" "}
@@ -78,7 +78,7 @@ const Dashboard = () => {
             <p className="text-gray-600 font-medium">
               You just added new tasks 👋
             </p>
-            <div className="flex justify-between py-2">
+            <div className="flex justify-between py-1">
               <p className="text-gray-400">You have 10 tasks</p>
               <p className="text-gray-400">11 Aug</p>
             </div>{" "}
