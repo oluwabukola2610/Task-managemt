53d5efa27e0e678c5ba37fbea5d696dbacbe5549
oluwabukola2610 <roheemohmudashir@gmail.com>
2023-08-28 12:46:20 +0100

    new changes

--- src/component/Calender.tsx
+++ src/component/Calender.tsx
@@ -1,208 +1,32 @@
+import moment from "moment";
+import Calendar from "react-calendar";
+import "react-calendar/dist/Calendar.css";
 
 const Calender = () => {
-  return (
-<div className="flex flex-col  p-2 shadow-[0_3px_10px_rgb(0,0,0,0.2)] bg-white/75 font-semibold text-center max-h-[400px] overflow-y-auto">
-          <div className="flex items-center justify-around px-4 py-6">
-            <button className="p-4 rounded-md bg-pink-200 text-pink-600">
-              <svg
-                className="w-4 h-4 stroke-current"
-                fill="none"
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                strokeWidth="4"
-                stroke="currentColor"
-                viewBox="0 0 24 24"
-              >
-                <path d="M15 19l-7-7 7-7"></path>
-              </svg>
-            </button>
-            <div className="text-lg">May, 2020</div>
-            <button className="p-4 rounded-md bg-pink-200 text-pink-600">
-              <svg
-                className="w-4 h-4 stroke-current"
-                fill="none"
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                strokeWidth="4"
-                stroke="currentColor"
-                viewBox="0 0 24 24"
-              >
-                <path d="M9 5l7 7-7 7"></path>
-              </svg>
-            </button>
-          </div>
-          <div className="grid grid-cols-7 grid-col-dense grid-rows-6 p-6 gap-1">
-            <div className="text-pink-600">Mon</div>
-            <div className="text-pink-600">Tue</div>
-            <div className="text-pink-600">Wed</div>
-            <div className="text-pink-600">Thu</div>
-            <div className="text-pink-600">Fri</div>
-            <div className="text-pink-600">Sat</div>
-            <div className="text-pink-600">Sun</div>
-            <a
-              href="#"
-              className="hover:bg-pink-100 rounded-md p-2 text-gray-500"
-            >
-              27
-            </a>
-            <a
-              href="#"
-              className="hover:bg-pink-100 rounded-md p-2 text-gray-500"
-            >
-              28
-            </a>
-            <a
-              href="#"
-              className="hover:bg-pink-100 rounded-md p-2 text-gray-500"
-            >
-              29
-            </a>
-            <a
-              href="#"
-              className="hover:bg-pink-100 rounded-md p-2 text-gray-500"
-            >
-              30
-            </a>
-            <a href="#" className="hover:bg-pink-100 rounded-md p-2">
-              1
-            </a>
-            <a href="#" className="hover:bg-pink-100 rounded-md p-2">
-              2
-            </a>
-            <a href="#" className="hover:bg-pink-100 rounded-md p-2">
-              3
-            </a>
-            <a href="#" className="hover:bg-pink-100 rounded-md p-2">
-              4
-            </a>
-            <a href="#" className="hover:bg-pink-100 rounded-md p-2">
-              5
-            </a>
-            <a href="#" className="hover:bg-pink-100 rounded-md p-2">
-              6
-            </a>
-            <a href="#" className="hover:bg-pink-100 rounded-md p-2">
-              7
-            </a>
-            <a href="#" className="hover:bg-pink-100 rounded-md p-2">
-              8
-            </a>
-            <a href="#" className="hover:bg-pink-100 rounded-md p-2">
-              9
-            </a>
-            <a href="#" className="hover:bg-pink-100 rounded-md p-2">
-              10
-            </a>
-            <a href="#" className="hover:bg-pink-100 rounded-md p-2">
-              11
-            </a>
-            <a href="#" className="hover:bg-pink-100 rounded-md p-2">
-              12
-            </a>
-            <a href="#" className="hover:bg-pink-100 rounded-md p-2">
-              13
-            </a>
-            <a href="#" className="hover:bg-pink-100 rounded-md p-2">
-              14
-            </a>
-            <a href="#" className="hover:bg-pink-100 rounded-md p-2">
-              15
-            </a>
-            <a href="#" className="hover:bg-pink-100 rounded-md p-2">
-              16
-            </a>
-            <a href="#" className="hover:bg-pink-100 rounded-md p-2">
-              17
-            </a>
-            <a href="#" className="hover:bg-pink-100 rounded-md p-2">
-              18
-            </a>
-            <a href="#" className="hover:bg-pink-100 rounded-md p-2">
-              19
-            </a>
-            <a href="#" className="hover:bg-pink-100 rounded-md p-2">
-              20
-            </a>
-            <a href="#" className="hover:bg-pink-100 rounded-md p-2">
-              21
-            </a>
-            <a href="#" className="hover:bg-pink-100 rounded-md p-2">
-              22
-            </a>
-            <a
-              href="#"
-              className="hover:bg-pink-600 rounded-md p-2 bg-pink-500 text-white"
-            >
-              23
-            </a>
-            <a href="#" className="hover:bg-pink-100 rounded-md p-2">
-              24
-            </a>
-            <a href="#" className="hover:bg-pink-100 rounded-md p-2">
-              25
-            </a>
-            <a href="#" className="hover:bg-pink-100 rounded-md p-2">
-              26
-            </a>
-            <a href="#" className="hover:bg-pink-100 rounded-md p-2">
-              27
-            </a>
-            <a href="#" className="hover:bg-pink-100 rounded-md p-2">
-              28
-            </a>
-            <a href="#" className="hover:bg-pink-100 rounded-md p-2">
-              29
-            </a>
-            <a href="#" className="hover:bg-pink-100 rounded-md p-2">
-              30
-            </a>
-            <a href="#" className="hover:bg-pink-100 rounded-md p-2">
-              31
-            </a>
-            <a
-              href="#"
-              className="hover:bg-pink-100 rounded-md p-2 text-gray-500"
-            >
-              1
-            </a>
-            <a
-              href="#"
-              className="hover:bg-pink-100 rounded-md p-2 text-gray-500"
-            >
-              2
-            </a>
-            <a
-              href="#"
-              className="hover:bg-pink-100 rounded-md p-2 text-gray-500"
-            >
-              3
-            </a>
-            <a
-              href="#"
-              className="hover:bg-pink-100 rounded-md p-2 text-gray-500"
-            >
-              4
-            </a>
-            <a
-              href="#"
-              className="hover:bg-pink-100 rounded-md p-2 text-gray-500"
-            >
-              5
-            </a>
-            <a
-              href="#"
-              className="hover:bg-pink-100 rounded-md p-2 text-gray-500"
-            >
-              6
-            </a>
-            <a
-              href="#"
-              className="hover:bg-pink-100 rounded-md p-2 text-gray-500"
-            >
-              7
-            </a>
-          </div>
-        </div>  )
-}
+  const date = new Date();
+  const formattedDate = moment(date).format("MMMM Do YYYY");
+  interface Calender {
+    date: Date;
+    view: string;
+  }
+  // Function to add custom CSS class to the active date
+  const tileClassName = ({ date, view }: Calender) => {
+    if (view === "month" && date.toDateString() === new Date().toDateString()) {
+      return "text-black rounded-md bg-pink-200 font-bold hover::bg-pink-100"; // Your custom CSS class
+    }
+    return null;
+  };
 
-export default Calender
+  return (
+    <div>
+      <Calendar
+        value={date}
+        tileClassName={tileClassName}
+        className=" border-none p-3 "
+      />
+      <p className="text-center py-2 font-semibold text-gray-400">Date: {formattedDate}</p>
+    </div>
+  );
+};
+
+export default Calender;
--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -1,6 +1,6 @@
 import workmode from "../assets/Working-pana-removebg.png";
-import Avatar from "../assets/Avatar.svg";
 import Calender from "../component/Calender";
+import TaskList from "../component/TaskList";
 
 interface TaskData {
   taskLeft: number;
@@ -88,77 +88,7 @@ const Dashboard = () => {
       </div>
       {/* task part */}
       <div className="grid grid-cols-1 lg:grid-cols-[1fr,370px] gap-4 ">
-        <div className="flex flex-col space-y-4 p-3 shadow-[0_3px_10px_rgb(0,0,0,0.2)] bg-white/75">
-          <h1 className="font-semibold py-2 text-gray-500">Today Task</h1>
-          <div className="flex items-center justify-between">
-            <span className="flex space-x-2 items-center">
-              <div className="h-2 w-2 bg-red-500 rounded-full"></div>
-              <p>Learn React</p>
-            </span>
-            <p className="text-gray-400">12 min</p>
-          </div>
-          <div className="flex items-center space-x-4">
-            <div className="border-r border-gray-300 h-full"></div>
-            <p className="text-gray-400">Create a simple website with reacts</p>
-          </div>
-
-          <div className="flex items-center justify-between">
-            <span className="flex space-x-2 items-center">
-              <div className="h-2 w-2 bg-green-500 rounded-full"></div>
-              <p>Do a responsive landing page</p>
-            </span>
-            <p className="text-gray-400">45 min</p>
-          </div>
-          <div className="flex items-center space-x-4">
-            <div className="border-r border-gray-300 h-full"></div>
-            <p className="text-gray-400">Project meting with John</p>
-            <div className="flex  items-center justify-center space-x-3">
-              <div className="avatar">
-                <div className="w-10 rounded-full">
-                  <img src="https://images.unsplash.com/photo-1531427186611-ecfd6d936c79?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=634&h=634&q=80" />
-                </div>
-              </div>
-              <p className="">John Doe (client)</p>{" "}
-            </div>
-          </div>
-          <div className="flex items-center justify-between">
-            <span className="flex space-x-2 items-center">
-              <div className="h-2 w-2 bg-yellow-500 rounded-full"></div>
-              <h1 className="font-seimbold">
-                Create a new React project for client
-              </h1>
-            </span>
-            <p className="text-gray-400">1 hr ago</p>
-          </div>
-          <div className="flex items-center space-x-4">
-            <div className="border-r border-gray-300 h-full"></div>
-            <h1 className="text-gray-500">5 team member </h1>
-            <div className="avatar-group -space-x-6">
-              <div className="avatar">
-                <div className="w-12">
-                  <img src={Avatar} alt="" />{" "}
-                </div>
-              </div>
-              <div className="avatar">
-                <div className="w-12">
-                  <img src="https://images.unsplash.com/photo-1531427186611-ecfd6d936c79?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=634&h=634&q=80" />
-                </div>
-              </div>
-              <div className="avatar">
-                <div className="w-12">
-                  <img src="https://images.unsplash.com/photo-1531427186611-ecfd6d936c79?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=634&h=634&q=80" />
-                </div>
-              </div>
-              <div className="avatar">
-                <div className="w-12">
-                  <img src="https://images.unsplash.com/photo-1531427186611-ecfd6d936c79?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=634&h=634&q=80" />
-                </div>
-              </div>
-            </div>
-          </div>
-
-          {/* ... more divs */}
-        </div>
+        <TaskList />
         {/* calender */}
         <Calender />
       </div>
